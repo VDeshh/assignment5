f2721dd8a2987b01331dccc035c1bfd604f5b99b
UBC Student <vdesh@students.cs.ubc.ca>
2023-07-25 11:53:15 -0700

    update

--- src/components/card.test.js
+++ src/components/card.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'
-import { Card } from './Card';
+import { Card } from './card';
 
 test('Edit and Save buttons work as expected', () => {
   const card = {
