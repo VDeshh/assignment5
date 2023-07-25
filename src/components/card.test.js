62658245d8d3d47909c9ded287ad0ef2bf074ded
UBC Student <vdesh@students.cs.ubc.ca>
2023-07-25 11:35:30 -0700

    jtest

--- src/components/card.test.js
+++ src/components/card.test.js
@@ -7,28 +7,35 @@ test('Edit and Save buttons work as expected', () => {
   const card = {
     _id: '1',
     name: 'Test Card',
-    description: 'This is a test card.',
+    description: '',
     price: '100',
     image: 'test.jpg',
   };
 
-  const { getByText, getByRole, queryByRole } = render(<Card card={card} deleteHandler={() => {}} editHandler={() => {}} />);
+  const { getByText, getAllByRole, queryByRole } = render(<Card card={card} deleteHandler={() => {}} editHandler={() => {}} />);
 
+  // Expect the Edit button to be in the document initially
   const editButton = getByText('Edit');
   expect(editButton).toBeInTheDocument();
 
+  // Click the Edit button
   fireEvent.click(editButton);
 
+  // Expect the Save button to now be in the document
   const saveButton = getByText('Save');
   expect(saveButton).toBeInTheDocument();
 
-  expect(getByRole('textbox', { name: '' })).toBeInTheDocument(); // For name and price
-  expect(queryByRole('textbox', { name: 'textarea' })).toBeInTheDocument(); // For description
+  expect(getAllByRole('textbox', { name: '' })).toHaveLength(3); // For name and price
+  expect(queryByRole('textbox', { name: 'textarea' })).toBeNull();
 
-  fireEvent.click(saveButton);
 
-  expect(saveButton).not.toBeInTheDocument();
-  expect(editButton).toBeInTheDocument();
+fireEvent.click(saveButton);
+
+const editButtonAfterSave = getByText('Edit');
+
+expect(saveButton).not.toBeInTheDocument();
+expect(editButtonAfterSave).toBeInTheDocument();
+
 
   expect(queryByRole('textbox', { name: '' })).not.toBeInTheDocument(); // For name and price
   expect(queryByRole('textbox', { name: 'textarea' })).not.toBeInTheDocument(); // For description
